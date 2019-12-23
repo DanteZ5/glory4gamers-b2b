5d21622b3a26ea5a06ad860293d5a9ef5c254e9f
EffyDante <dplanterose@gmail.com>
2019-12-23 16:52:48 +0100

    going on

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,27 @@
 //= require rails-ujs
 //= require_tree .
+$(document).on('turbolinks:load', function(){
+  var logo = $('.customer-logos');
+  if (logo.length) {
+    $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }]
+    });
+  }
+});
