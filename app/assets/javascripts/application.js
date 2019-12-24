bebbd696f6eab548a64bc23612493bdc504d96ff
EffyDante <dplanterose@gmail.com>
2019-12-24 09:37:00 +0100

    home in progress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,27 +1,28 @@
 //= require rails-ujs
 //= require_tree .
-$(document).on('turbolinks:load', function(){
-  var logo = $('.customer-logos');
-  if (logo.length) {
-    $('.customer-logos').slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1000,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [{
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 4
-        }
-      }, {
-        breakpoint: 520,
-        settings: {
-          slidesToShow: 3
-        }
-      }]
-    });
-  }
-});
+console.log('coucou');
+
+var logo = $('.customer-logos');
+if (logo.length) {
+  $('.customer-logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 3
+      }
+    }]
+  });
+}
+
